cefd632696a40689501b15fda6f9839d3b75d847
k661301@kansai-u.ac.jp <k661301@kansai-u.ac.jp>
2022-08-03 11:33:24 +0900

    a

--- details/gui.h
+++ details/gui.h
@@ -1,10 +1,33 @@
 #pragma once
+#include <functional>
 #include "def.h"
 #include "details.h"
+#include "shape.h"
+#include "text.h"
 
 namespace dx_engine {
 	namespace gui {
+		template<class shape = rect>
 		class clickable_object {
+		private:
+
+		protected:
+			enum class cursor_info_t {
+				none,
+				hover,
+				pressed
+			}cursor_info;
+		public:
+
+		};
+
+		template<class shape = rect>
+		class button : public clickable_object<shape>{
+		private:
+
+		protected:
+
+		public:
 
 		};
 	}
